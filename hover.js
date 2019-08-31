9ed734d75ce28cedd1e0645a6810c583d774b6ac
senjoshua <senjoshu@gmail.com>
2019-08-31 12:40:51 -0400

    reformatted cards for projects

---
+++ hover.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+     $( ".card" ).hover(
+     function() {
+       $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+     }, function() {
+       $(this).removeClass('shadow-lg');
+     }
+   );
+   });
+   
+   
